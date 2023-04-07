d2698ca6a04b4ded713ed3039f3544c88dd19a97
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-07 14:37:14 +0200

    02_whyLoops

---
+++ 02_why_loops/theory.py
@@ -0,0 +1,54 @@
+"""
+Loops
+Why Loops?
+
+Before we get to writing our own loops, let’s explore what programming would be like if we couldn’t use loops.
+
+Let’s say we have a list of ingredients and we want to print every element in the list:
+"""
+ingredients = ["milk", "sugar", "vanilla extract", "dough", "chocolate"]
+"""
+If we only use print(), our program might look like this:
+
+print(ingredients[0])
+print(ingredients[1])
+print(ingredients[2])
+print(ingredients[3])
+print(ingredients[4])
+
+The output would be:
+
+milk
+sugar
+vanilla extract
+dough
+chocolate
+
+That’s still manageable, We’re writing 5 print() statements (or copying and pasting a few times). Now imagine if we come
+back to this program and our list had 10, or 24601, or … 100,000,000 elements? It would take an extremely long time and
+by the end, we could still end up with inconsistencies and mistakes.
+
+Don’t dwell too long on this tedious scenario — we’ll learn how loops can help us out in the next exercise. For now,
+let’s gain an appreciation for loops.
+Instructions
+1.
+
+Using 10 print() statements, print out: "This can be so much easier with loops!".
+
+If you already have an idea of how to use loops (or have searched up how to do it) we’re still expecting 10 individual
+print() statements.
+
+If for some reason you still are not passing the tests, make sure all your print() statements have the same text "This
+can be so much easier with loops!".
+
+Copy and paste is your best friend here 😉
+Concept Review
+Want to quickly review some of the concepts you’ve been learning? Take a look at this material's cheatsheet!
+Community Forums
+
+Here are some helpful links to the top questions asked by coders about this exercise:
+
+    What kind of loop operations are available in Python?
+
+Still have questions? View this exercise's thread in the Codecademy Forums.
+"""
